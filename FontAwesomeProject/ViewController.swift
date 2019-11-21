0f1c596648e514cba8fa63815776b84d100fb2ce
城間海月 <mizushiro426426@gmail.com>
2019-11-21 13:07:16 +0800

    Initial Commit

---
+++ FontAwesomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FontAwesomeProject
+//
+//  Created by 城間海月 on 2019/11/21.
+//  Copyright © 2019 Mizuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
